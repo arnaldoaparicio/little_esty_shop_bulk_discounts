138f1ff66d4777a05826a7ff725b800b424e847a
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-17 09:08:04 -0500

    Add update method

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,15 +6,25 @@ class BulkDiscountsController < ApplicationController
   def show 
     @bulk_discount = BulkDiscount.find(params[:id])
   end
-
+  
   def new 
     @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def edit 
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
   
   def create 
     bulk_discount = BulkDiscount.new(discount_params)
     bulk_discount.save
     redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+  end
+
+  def update
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.update(discount_params)
+    redirect_to merchant_bulk_discount_path(params[:merchant_id], bulk_discount)
   end
 
   def destroy
